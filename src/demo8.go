cfbe4e62a02ee92242586aa985d77376f91aee02
朱永康 <763960267@qq.com>
2014-08-24 17:46:36 +0800

    add

--- src/demo8.go
+++ src/demo8.go
@@ -45,4 +45,12 @@ LABEL1:
 	}
 
 	fmt.Println(s1)
+
+	m1 := map[int]string{1: "a", 2: "b", 3: "c", 4: "d", 5: "e"}
+	fmt.Println(m1)
+	m2 := make(map[string]int)
+	for k, v := range m1 {
+		m2[v] = k
+	}
+	fmt.Println(m2)
 }
